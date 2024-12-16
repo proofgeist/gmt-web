6f9ba45bf663d709fb33b64fe367d0f33c4e736b
Chris Corsi <chris.corsi@proofgeist.com>
2024-12-16 11:37:27 -0500

    Display mods, move footer

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,3 +1,4 @@
+
 import {
   Anchor,
   AppShellFooter,
@@ -25,7 +26,7 @@ import Services from "./services";
 export default function Home() {
   return (
     <>
-      <Container mt="2rem" h={"100vh"}>
+      <Container mt="2rem">
         {/* Hero Section */}
         <Card bg={"gray.1"}>
           <Container>
@@ -70,75 +71,6 @@ export default function Home() {
 
         {/* Services Section */}
         <Services />
-
-        {/* Footer Section */}
-        <AppShellFooter>
-          <Box component="section" py={"30px"} bg={"#003366"} c={"white"}>
-            <Container>
-              <Grid align="center">
-                <GridCol span={8}>
-                  <Stack gap="md">
-                    <Title order={4} c="white">
-                      Contact Us:
-                    </Title>
-                    <Flex direction={{ base: "row", sm: "column" }} gap="md">
-                      <Anchor href="tel:1-212-717-7754" c="white">
-                        <Group gap="xs">
-                          <IconPhone size={20} />
-                          <Text c="white" visibleFrom="sm">
-                            Phone: 1-212-717-7754
-                          </Text>
-                        </Group>
-                      </Anchor>
-                      <Anchor
-                        href="mailto:GMT-bookings@globalmarinetransportation.com"
-                        c="white"
-                      >
-                        <Group gap="xs">
-                          <IconMail size={20} />
-                          <Text c="white" visibleFrom="sm">
-                            GMT-bookings@globalmarinetransportation.com
-                          </Text>
-                        </Group>
-                      </Anchor>
-                    </Flex>
-                  </Stack>
-                </GridCol>
-                <GridCol span={4}>
-                  <Stack gap="md">
-                    <Title order={4} c="white">
-                      Follow us:
-                    </Title>
-                    <Flex direction={{ base: "row", sm: "column" }} gap="md">
-                      <Anchor
-                        href="https://www.instagram.com/globalmarinetransportation/"
-                        target="_blank"
-                        underline="hover"
-                        c="white"
-                      >
-                        <Group gap="xs">
-                          <IconBrandInstagram size={20} />
-                          <Text visibleFrom="sm">Instagram</Text>
-                        </Group>
-                      </Anchor>
-                      <Anchor
-                        href="https://www.linkedin.com/company/global-marine-transportation-inc/?viewAsMember=true"
-                        target="_blank"
-                        underline="hover"
-                        c="white"
-                      >
-                        <Group gap="xs">
-                          <IconBrandLinkedin size={20} />
-                          <Text visibleFrom="sm">LinkedIn</Text>
-                        </Group>
-                      </Anchor>
-                    </Flex>
-                  </Stack>
-                </GridCol>
-              </Grid>
-            </Container>
-          </Box>
-        </AppShellFooter>
       </Container>
     </>
   );
