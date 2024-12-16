579a1ef451cecdb7466615a2474e29482b472243
Chris Corsi <chris.corsi@proofgeist.com>
2024-12-15 23:43:28 -0500

    Mod app logo

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -1,6 +1,14 @@
 import { IconInfinity } from "@tabler/icons-react";
+import { Group, Image, Title } from "@mantine/core";
 import React from "react";
 
 export default function AppLogo() {
-  return <IconInfinity size={32} />;
+  return (
+    <Group gap="xs">
+      <Image src="/gmt_logo.png" alt="ProofKit" width={32} height={32} />
+      <Title order={2} c={"#003366"}>
+        Global Marine Transportation
+      </Title>
+    </Group>
+  );
 }
