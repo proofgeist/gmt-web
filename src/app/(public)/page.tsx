4efc38bbafa74794441d5eceff1323432252b614
Chris Corsi <chris.corsi@proofgeist.com>
2024-12-16 00:01:17 -0500

    Added resposivity

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -81,52 +81,58 @@ export default function Home() {
                     <Title order={4} c="white">
                       Contact Us:
                     </Title>
-                    <Anchor href="tel:1-212-717-7754" c="white">
-                      <Group gap="xs">
-                        <IconPhone size={20} />
-                        <Text c="white">Phone: 1-212-717-7754</Text>
-                      </Group>
-                    </Anchor>
-                    <Anchor
-                      href="mailto:GMT-bookings@globalmarinetransportation.com"
-                      c="white"
-                    >
-                      <Group gap="xs">
-                        <IconMail size={20} />
-                        <Text c="white">
-                          GMT-bookings@globalmarinetransportation.com
-                        </Text>
-                      </Group>
-                    </Anchor>
+                    <Flex direction={{ base: "row", sm: "column" }} gap="md">
+                      <Anchor href="tel:1-212-717-7754" c="white">
+                        <Group gap="xs">
+                          <IconPhone size={20} />
+                          <Text c="white" visibleFrom="sm">
+                            Phone: 1-212-717-7754
+                          </Text>
+                        </Group>
+                      </Anchor>
+                      <Anchor
+                        href="mailto:GMT-bookings@globalmarinetransportation.com"
+                        c="white"
+                      >
+                        <Group gap="xs">
+                          <IconMail size={20} />
+                          <Text c="white" visibleFrom="sm">
+                            GMT-bookings@globalmarinetransportation.com
+                          </Text>
+                        </Group>
+                      </Anchor>
+                    </Flex>
                   </Stack>
                 </GridCol>
                 <GridCol span={4}>
                   <Stack gap="md">
                     <Title order={4} c="white">
-                      Follow us on:
+                      Follow us:
                     </Title>
-                    <Anchor
-                      href="https://www.instagram.com/globalmarinetransportation/"
-                      target="_blank"
-                      underline="hover"
-                      c="white"
-                    >
-                      <Group gap="xs">
-                        <IconBrandInstagram size={20} />
-                        <Text>Instagram</Text>
-                      </Group>
-                    </Anchor>
-                    <Anchor
-                      href="https://www.linkedin.com/company/global-marine-transportation-inc/?viewAsMember=true"
-                      target="_blank"
-                      underline="hover"
-                      c="white"
-                    >
-                      <Group gap="xs">
-                        <IconBrandLinkedin size={20} />
-                        <Text>LinkedIn</Text>
-                      </Group>
-                    </Anchor>
+                    <Flex direction={{ base: "row", sm: "column" }} gap="md">
+                      <Anchor
+                        href="https://www.instagram.com/globalmarinetransportation/"
+                        target="_blank"
+                        underline="hover"
+                        c="white"
+                      >
+                        <Group gap="xs">
+                          <IconBrandInstagram size={20} />
+                          <Text visibleFrom="sm">Instagram</Text>
+                        </Group>
+                      </Anchor>
+                      <Anchor
+                        href="https://www.linkedin.com/company/global-marine-transportation-inc/?viewAsMember=true"
+                        target="_blank"
+                        underline="hover"
+                        c="white"
+                      >
+                        <Group gap="xs">
+                          <IconBrandLinkedin size={20} />
+                          <Text visibleFrom="sm">LinkedIn</Text>
+                        </Group>
+                      </Anchor>
+                    </Flex>
                   </Stack>
                 </GridCol>
               </Grid>
--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -6,8 +6,11 @@ export default function AppLogo() {
   return (
     <Group gap="xs">
       <Image src="/gmt_logo.png" alt="ProofKit" width={32} height={32} />
-      <Title order={2} c={"#003366"}>
+      <Title visibleFrom="sm" order={2} c={"brand"}>
         Global Marine Transportation
+      </Title>
+      <Title hiddenFrom="sm" order={2} c={"brand"}>
+        GMT
       </Title>
     </Group>
   );
