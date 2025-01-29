c3eb46ca58578630a48974d5e8447ac81e8f2a30
Chris Corsi <chris.corsi@proofgeist.com>
2025-01-29 00:15:39 -0500

    validate phone

--- src/app/auth/signup/schema.ts
+++ src/app/auth/signup/schema.ts
@@ -3,14 +3,19 @@ import { z } from "zod";
 export const signupSchema = z
   .object({
     email: z.string().email(),
-    phoneNumber: z.string().min(11),
+    phoneNumber: z.string().transform((num) => num.replace(/[^\d]/g, "")),
     password: z.string().min(8),
     confirmPassword: z.string(),
   })
-  .refine((data) => data.phoneNumber.length < 11, {
+  .refine((data) => data.phoneNumber.length >= 11, {
     path: ["phoneNumber"],
-    message: "Phone number must be at least 11 characters long - include country code",
+    message:
+      "Phone number must be at least 11 characters long - include country code",
   })
+  .transform((data) => ({
+    ...data,
+    phoneNumber: `+${data.phoneNumber}`,
+  }))
   .refine((data) => data.password === data.confirmPassword, {
     path: ["confirmPassword"],
     message: "Passwords do not match",
