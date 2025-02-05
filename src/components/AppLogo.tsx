776282e52c2f5305e71e1d2565319ef365ef31c4
Chris Corsi <chris.corsi@proofgeist.com>
2025-02-05 16:10:51 -0500

    remove title

--- src/components/AppLogo.tsx
+++ src/components/AppLogo.tsx
@@ -14,12 +14,7 @@ export default function AppLogo() {
         radius={"md"}
         fit="contain"
       />
-      <Title visibleFrom="md" order={2} c={"brand"}>
-        GLOBAL MARINE TRANSPORTATION
-      </Title>
-      <Title hiddenFrom="md" order={2} c={"brand"}>
-        GMT
-      </Title>
+
     </Group>
   );
 }
