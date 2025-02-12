1cbc0c89912b86911d15f52b920734a80b3fca83
Chris Corsi <chris.corsi@proofgeist.com>
2025-02-12 11:23:56 -0500

    add font, user menu hiding

--- src/components/AppShell/public/PublicHeader.tsx
+++ src/components/AppShell/public/PublicHeader.tsx
@@ -4,5 +4,5 @@ import { Header } from "../internal/Header";
 import { publicRoutes } from "@/app/navigation";
 
 export function PublicHeader() {
-  return <Header routes={publicRoutes} textColor="white" hideLogo={true} />;
+  return <Header routes={publicRoutes} textColor="white" hideLogo={true} hideUserMenu={true} />;
 }
---
+++ src/config/theme/fonts.ts
@@ -0,0 +1,8 @@
+import { Yellowtail } from "next/font/google";
+
+export const yellowtail = Yellowtail({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-yellowtail",
+});
