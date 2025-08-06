b0173dfae0ab0760f930e70421ec8e31ffb97a65
Chris Corsi <chris.corsi@proofgeist.com>
2025-08-06 14:23:05 -0400

    linting

--- src/app/(protected)/actions.tsx
+++ src/app/(protected)/actions.tsx
@@ -17,14 +17,16 @@ export const getShipmentByTypeAction = authedActionClient
     try {
       const { type } = parsedInput;
 
-      if (type === "active") {
-        return getActiveShipmentsAction({ ctx }).then((data) => data?.data);
-      } else if (type === "pending") {
-        return getPendingShipmentsAction({ ctx }).then((data) => data?.data);
-      } else if (type === "completed") {
-        return getPastShipmentsAction({ ctx }).then((data) => data?.data);
+      switch (type) {
+        case "active":
+          return getActiveShipmentsAction({ ctx }).then((data) => data?.data);
+        case "pending":
+          return getPendingShipmentsAction({ ctx }).then((data) => data?.data);
+        case "completed":
+          return getPastShipmentsAction({ ctx }).then((data) => data?.data);
+        default:
+          throw new Error(`Unhandled shipment type`);
       }
-      throw new Error(`Invalid shipment type: ${type}`);
     } catch (error) {
       throw error instanceof Error ? error : (
           new Error("Failed to fetch shipments")
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -3,29 +3,27 @@ import LoginForm from "./login-form";
 export default async function Page({
   searchParams,
 }: {
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+  searchParams: Promise<{ error?: string }>;
 }) {
   const { error } = await searchParams;
-  
 
   return (
     <Container size={420} my={40}>
       <Stack>
+        <Title ta="center">Welcome back!</Title>
+        <Text c="dimmed" size="sm" ta="center" mt={5}>
+          Do not have an account yet?{" "}
+          <Anchor size="sm" component="a" href="/auth/signup">
+            Create account
+          </Anchor>
+        </Text>
 
-      <Title ta="center">Welcome back!</Title>
-      <Text c="dimmed" size="sm" ta="center" mt={5}>
-        Do not have an account yet?{" "}
-        <Anchor size="sm" component="a" href="/auth/signup">
-          Create account
-        </Anchor>
-      </Text>
-
-      <LoginForm />
-      {error && (
-        <Alert color="red" title="Error">
-          {error}
-        </Alert>
-      )}
+        <LoginForm />
+        {error && (
+          <Alert color="red" title="Error">
+            {error}
+          </Alert>
+        )}
       </Stack>
     </Container>
   );
