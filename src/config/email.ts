da0ea6f9ebe84d2195dfd3e92fe0934284ec58dc
Chris Corsi <chris.corsi@proofgeist.com>
2025-09-26 08:46:58 -0400

    update emails

--- src/config/email.ts
+++ src/config/email.ts
@@ -1,8 +1,7 @@
-export const DEFAULT_INBOX = "gmt-bookings@globalmarinetransportation.com";
+export const DEFAULT_INBOX = "gmt-bookings@mygmt.com";
 export const DEFAULT_STEW_INBOX = "stewart.feuer@mygmt.com";
 export const DEFAULT_NATALY_INBOX = "nataly@mygmt.com";
 export const DEFAULT_MANAGER_INBOX = [DEFAULT_STEW_INBOX, DEFAULT_NATALY_INBOX];
-export const DEFAULT_SIGNUP_EMAIL = "MyGMT <welcome@signup.mygmt.com>";
-export const DEFAULT_RELEASE_INBOX =
-  "gmt-release@globalmarinetransportation.com";
+export const DEFAULT_SIGNUP_EMAIL = "MyGMT <welcome@mygmt.com>";
+export const DEFAULT_RELEASE_INBOX = "gmt-release@mygmt.com";
   export const DEFAULT_FROM_EMAIL = "MyGMT <gmt-bookings@mygmt.com>";
